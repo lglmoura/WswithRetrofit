a1acae104dfc10ee216196b734f35dcd8d137a8d
Luiz Gustavo Lourenco Moura <lglmoura@gmail.com>
2018-08-13 20:09:34 -0300

    Criando consulta de Seguidores

--- app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/MainActivity.java
@@ -1,10 +1,14 @@
 package br.edu.iff.pooa20181.wswithretrofit;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.List;
 
 import br.edu.iff.pooa20181.wswithretrofit.interfaceRetrofit.iRetroFitGithub;
 import br.edu.iff.pooa20181.wswithretrofit.model.Usuario;
@@ -14,10 +18,18 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setMessage("Carregando...");
+        dialog.setCancelable(false);
+
+
 
         Button btUsuario = (Button) findViewById(R.id.btUsuario);
 
@@ -27,9 +39,15 @@ public class MainActivity extends AppCompatActivity {
                 iRetroFitGithub githubUser = iRetroFitGithub.retrofit.create(iRetroFitGithub.class);
                 final Call<Usuario> call = githubUser.getUsuario("longhibianca");
 
+                dialog.show();
+
                 call.enqueue(new Callback<Usuario>() {
                     @Override
                     public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+
+                        if(dialog.isShowing())
+                            dialog.dismiss();
+
                         int code = response.code();
                         if (code==200){
                             Usuario usuario = response.body();
@@ -40,7 +58,42 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(Call<Usuario> call, Throwable t) {
 
+                        if(dialog.isShowing())
+                            dialog.dismiss();
+
                         Toast.makeText(getBaseContext(),"Falha : ",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
+
+        Button btSeguidores = (Button) findViewById(R.id.btSeguidores);
+
+        btSeguidores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iRetroFitGithub githubUser = iRetroFitGithub.retrofit.create(iRetroFitGithub.class);
+                final Call<List<Usuario>> call = githubUser.getSeguidores("lglmoura");
+                dialog.show();
+                call.enqueue(new Callback<List<Usuario>>() {
+                    @Override
+                    public void onResponse(Call<List<Usuario>> call, Response<List<Usuario>> response) {
+                        if(dialog.isShowing())
+                            dialog.dismiss();
+                        List<Usuario> lista = response.body();
+                        for(Usuario usuario : lista){
+                            Log.d("LLLUUUIIIZZZZ", usuario.getLogin());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Usuario>> call, Throwable t) {
+
+                        if(dialog.isShowing())
+                            dialog.dismiss();
+
+                        Toast.makeText(getBaseContext(),"Flaha : ",Toast.LENGTH_LONG).show();
 
                     }
                 });
--- app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/interfaceRetrofit/iRetroFitGithub.java
+++ app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/interfaceRetrofit/iRetroFitGithub.java
@@ -1,5 +1,7 @@
 package br.edu.iff.pooa20181.wswithretrofit.interfaceRetrofit;
 
+import java.util.List;
+
 import br.edu.iff.pooa20181.wswithretrofit.model.Usuario;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -11,6 +13,9 @@ public interface iRetroFitGithub {
 
     @GET("/users/{usuario}")
     Call<Usuario> getUsuario(@Path("usuario") String usuario);
+
+    @GET("/users/{usuario}/followers")
+    Call<List<Usuario>> getSeguidores(@Path("usuario") String usuario);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.github.com/")
