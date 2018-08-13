846c5076914169ca074f6467119d773f62c8d023
Luiz Gustavo Lourenco Moura <lglmoura@gmail.com>
2018-08-13 19:55:30 -0300

    alterando o Main

--- app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/MainActivity.java
@@ -1,7 +1,16 @@
 package br.edu.iff.pooa20181.wswithretrofit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import br.edu.iff.pooa20181.wswithretrofit.interfaceRetrofit.iRetroFitGithub;
+import br.edu.iff.pooa20181.wswithretrofit.model.Usuario;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btUsuario = (Button) findViewById(R.id.btUsuario);
+
+        btUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iRetroFitGithub githubUser = iRetroFitGithub.retrofit.create(iRetroFitGithub.class);
+                final Call<Usuario> call = githubUser.getUsuario("longhibianca");
+
+                call.enqueue(new Callback<Usuario>() {
+                    @Override
+                    public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                        int code = response.code();
+                        if (code==200){
+                            Usuario usuario = response.body();
+                            Toast.makeText(getBaseContext(),"Nome : "+usuario.getName(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Usuario> call, Throwable t) {
+
+                        Toast.makeText(getBaseContext(),"Falha : ",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
+
+
+
+
+
+
     }
 }
--- app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/model/Usuario.java
+++ app/src/main/java/br/edu/iff/pooa20181/wswithretrofit/model/Usuario.java
@@ -5,4 +5,28 @@ public class Usuario {
     int id;
     String name;
     String login;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }
